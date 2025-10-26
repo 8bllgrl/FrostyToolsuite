9f856e70157d938d5fb7653df23a6435ca77a348
8bllgrl <102974548+8bllgrl@users.noreply.github.com>
2025-10-25 21:53:06 -0600

    minor change to EaLayer3

--- Plugins/SoundEditorPlugin/Playback/EALayer3.cs
+++ Plugins/SoundEditorPlugin/Playback/EALayer3.cs
@@ -2,21 +2,19 @@
 using System.Runtime.InteropServices;
 
 namespace SoundEditorPlugin.Playback
-{
+{ 
     public static class EALayer3
     {
+        [DllImport("../thirdparty/ealayer3.dll")]
+        public static extern void Decode([MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)] byte[] buffer, int length, EALayer3.AudioCallback callback);
+
         public delegate void AudioCallback([MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)] short[] data, int count, EALayer3.StreamInfo info);
 
-        [StructLayout(LayoutKind.Sequential)]
         public struct StreamInfo
         {
             public int streamIndex;
             public int numChannels;
             public int sampleRate;
         }
-
-        [DllImport("../thirdparty/ealayer3.dll", EntryPoint = "Decode")]
-        public static extern void Decode([MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)] byte[] buffer, int length, EALayer3.AudioCallback callback);
     }
-
 }
